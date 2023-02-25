dc2e511356a4dfa241d10e7571663ba52f45e7d3
Thigo <thiago.l17@yahoo.com>
2023-02-25 19:36:05 -0300

    feat(): Working with SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Head from 'next/head'
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
 
@@ -28,6 +29,11 @@ export default function Home({ products }: HomeProps) {
   });
 
   return (
+    <>
+    <Head>
+      <title> Home | Ignite Shop </title>
+    </Head>
+
     <HomeContainer ref={sliderRef} className='keen-slider'> 
     {products.map(product => {
       return (
@@ -46,6 +52,8 @@ export default function Home({ products }: HomeProps) {
       )
     })}
     </HomeContainer>
+
+    </>
   )
 }
 
