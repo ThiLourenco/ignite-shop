8edf0a7f2a14679a1f6eb2fa2c9ae3b1fc80ae81
Thigo <thiago.l17@yahoo.com>
2023-02-25 19:11:12 -0300

    feat(): successful structure

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,24 @@
+import { ImageContainer } from "@/styles/pages/product";
+import { SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success () {
   return (
-    <h1>Success</h1>
+    <SuccessContainer> 
+
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+      Uhuul <strong>Thiago Lourenço</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href='/'>
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
